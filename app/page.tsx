59b456b5a6adf84b25e66c8d408e42119d23298f
Nirav <niravpatelp129@gmail.com>
2024-03-31 22:40:45 -0400

    Add button functionality and update styles

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,11 @@ export default function Home() {
   // State to store the mouse position
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
+  const handleButtonPress = () => {
+    console.log('Button pressed');
+    alert('subscribed!');
+  };
+
   // This function will run when the mouse enters the main area
   const handleMouseEnter = () => {
     setIsHovered(true);
@@ -27,7 +32,7 @@ export default function Home() {
 
   return (
     <main
-      className='h-screen w-full flex justify-center items-center bg-gradient-to-r from-blue-500 to-pink-500'
+      className='h-screen w-full flex justify-center items-center bg-gradient-to-r from-blue-500 to-pink-500 bg-opacity-50'
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
       onMouseMove={handleMouseMove}
@@ -50,11 +55,35 @@ export default function Home() {
       )}
       <div
         className={`h-full w-full flex justify-center items-center transition-opacity duration-300 ${
-          isHovered ? 'opacity-50' : 'opacity-0'
+          isHovered ? 'opacity-100' : 'opacity-10'
         }`}
-        style={{ background: 'rgba(255, 255, 255, 0.5)' }}
+        // style={{ background: 'rgba(255, 255, 255, 0.5)' }}
       >
-        <h1 className='text-white text-4xl'>Coming Soon</h1>
+        <div style={{}}>
+          <h1 className='text-white text-[120px] font-extrabold font-segoe-ui'>Coming Soon</h1>
+          <div>
+            <p className='text-white text-center text-lg'>
+              In the meantime. Sign up for our monthly newsletter to stay up to date.
+            </p>
+            <div className='flex items-center justify-center w-full mt-20'>
+              {/* rounded border input */}
+              <input
+                className='rounded-full border border-white p-4 w-96 text-black focus:outline-none'
+                type='email'
+                placeholder='Email Address'
+              />
+
+              {/* submit button black with white text */}
+              <button
+                onClick={handleButtonPress}
+                className='bg-black text-white px-8 py-5 rounded-full ml-4 hover:opacity-80 
+              active:opacity-100'
+              >
+                Subscribe
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,11 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        'segoe-ui': ['Segoe UI', 'sans-serif'],
+      },
     },
   },
   plugins: [nextui()],
