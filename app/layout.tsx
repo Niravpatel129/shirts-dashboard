37490b5106e9a88ac8fce6a160a887761d98f1a7
Nirav <niravpatelp129@gmail.com>
2024-04-03 00:27:58 -0400

    Update favicon and page title

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,14 +13,14 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <head>
+        <title>Boloprint: Online Printing</title>
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' sizes='any' />
+        <link rel='icon' href='/favicon.ico' />
         <meta
           name='description'
           content='BoloPrint online customization services enable you to design and purchase top-quality custom clothing: from promotional apparel, personalized t-shirts, bespoke business attire, to unique custom-made gifts and more.'
         />
-        <title>Boloprint: Online Printing</title>
       </head>
       <body className={inter.className}>
         <NextUIProvider>{children}</NextUIProvider>
